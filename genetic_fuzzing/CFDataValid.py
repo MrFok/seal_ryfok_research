75c26efde7a15c76fe620947547f66560d6b0f1c
Ricky Fok <ricky.y.fok@gmail.com>
2024-02-16 15:38:23 -0800

    Added class to verify grammar performance on cf & non-cf files

---
+++ genetic_fuzzing/CFDataValid.py
@@ -0,0 +1,160 @@
+import io
+import sys
+from antlr4 import *
+from JSON5Lexer import JSON5Lexer
+from JSON5Parser import JSON5Parser
+
+import subprocess
+
+CF_FILES = {"DataPipeline-multiple-StringValue.json", 
+            "DirectoryServiceSimpleAD.json",
+            "DMSAuroraToS3FullLoadAndOngoingReplication.json",
+            "EMRClusterGangliaWithSparkOrS3backedHbase.json",
+            "EMRClusterWithAdditioanalSecurityGroups.json",
+            "NetworkLoadBalancerWithEIPs.json",
+            "SingleENIwithMultipleEIPs.json",
+            "SNSTopic.json",
+            "SQSFIFOQueue.json",
+            "SQSStandardQueue.json"
+            }
+
+NON_CF_FILES = {"BackgroundMediaPlayback.json",
+                "GoogleDataFeed.json",
+                "MapsMarker.json",
+                "NASAMarsWeather.json",
+                "OneSkyJasonSample2.json",
+                "OpenAIPetStoreSimple.json",
+                "RiotClientInstalls.json",
+                "RoboMakerOneBedroom.json",
+                "ShopifySectionData.json",
+                "SolarEnergeticParticle.json"
+                }
+
+CF_FILES_DIR = "cf_json\\"
+NON_CF_FILES_DIR = "non_cf_json\\"
+
+"""
+Create a CFDataValid class to validate current JSON5 implementation
+"""
+class CFDataValid():
+    def __init__(self):
+        self.cf_files = CF_FILES
+        self.non_cf_files = NON_CF_FILES
+        self.cf_states = {}
+        self.non_cf_states = {}
+
+    def validate_file(self, filePath, verbose=False):
+        """
+        Validates given file if it is a valid JSON and a valid CloudFormation template.
+
+        Args:
+            filePath (str): The path to the JSON5 file.
+            verbose (bool, optional): If True, prints additional information. Defaults to False.
+
+        Returns:
+            bool: True if the file is parsed successfully, False otherwise.
+        """
+        if verbose:
+            print(f'Validating file: "{filePath}"')
+        input_stream = FileStream(filePath)
+        lexer = JSON5Lexer(input_stream)
+        stream = CommonTokenStream(lexer)
+        parser = JSON5Parser(stream)
+
+        # Redirect stdout and stderr to buffer
+        old_stderr = sys.stderr
+        old_stdout = sys.stdout
+        strerr_buffer = io.StringIO()
+        strout_buffer = io.StringIO()
+        sys.stderr = strerr_buffer
+        sys.stdout = strout_buffer
+
+        tree = parser.json5()
+
+        # Restore stdout & stderr
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+        statements = strerr_buffer.getvalue() + strout_buffer.getvalue()
+
+        # Check if file was parsed successfully via JSON5.g4 grammar
+        if statements != "":
+            if verbose:
+                print("JSON5 Validation | FAIL")
+            return False
+
+        if verbose:
+            print("JSON5 Validation | PASS")
+        return True
+
+    def cf_validate(self, filePath, verbose=False):
+        """
+        Validates file adherance to CloudFormation template using cfn-lint.
+
+        cfn-lint runs with .cfnlintrc configuration file in the root directory.
+
+        Args:
+            filePath (str): The path to the CloudFormation template file.
+            verbose (bool, optional): If True, prints additional information about the validation process. Defaults to False.
+
+        Raises:
+            Exception: If the file is not a valid CloudFormation template.
+
+        Returns:
+            None
+        """
+
+        try:
+            subprocess.check_output(['cfn-lint', filePath])
+        except subprocess.CalledProcessError as e:
+            if verbose:
+                print(f'cfn-lint Validation | FAIL')
+                print(f'cfn-lint output: "{e.output.decode()}"')
+            return False
+
+        if verbose:
+            print(f'cfn-lint Validation | PASS')
+        return True
+    
+    def validate_parser(self, verbose=False):
+        """
+        Validates if the grammar is parsing our test CF files and non-CF files correctly.
+
+        Args:
+            verbose (bool): If True, prints the validation status for each file.
+
+        Returns:
+            dict: A dictionary of the validation status for each CF file.
+            dict: A dictionary of the validation status for each non-CF file.
+            int: The number of CF files that passed validation.
+            int: The number of non-CF files that passed validation
+        """
+
+        cf_pass = 0
+        non_cf_pass = 0
+        if verbose:
+            print("Validating Positive CloudFormation files...")
+        for file in self.cf_files:
+            status_parser = self.validate_file(CF_FILES_DIR + file)
+
+            # Status in CF set should be True
+            self.cf_states[file] = status_parser
+            if status_parser:
+                cf_pass += 1
+            if verbose:
+                print(f'{file[:12]}... | {"PASS - Parsed Successfully" if self.cf_states[file] else "FAIL - Parser Failed"}', end='')
+                print()
+
+        if verbose:
+            print("\nValidating Negative CloudFormation files...")
+        
+        for file in self.non_cf_files:
+            status_parser = self.validate_file(NON_CF_FILES_DIR + file)
+
+            # Status in non CF set should be False
+            self.non_cf_states[file] = status_parser
+            if status_parser:
+                non_cf_pass += 1
+            if verbose:
+                print(f'{file[:12]}... | {"PASS - Unable to Parse" if not self.non_cf_states[file] else "FAIL - Parsed Successfully"}')
+        
+        return self.cf_states, self.non_cf_states, cf_pass, non_cf_pass
